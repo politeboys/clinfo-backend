21db983bc34f108d3304613889b9151cd0cce6a6
Your Name <you@example.com>
2021-02-15 08:13:46 +0600

    ldjfl

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-const express = require("express")
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
 const app = express()
-let port = process.env.PORT || 4000;
+app.use(cors())
+app.use(bodyParser.json())
+const uri = "mongodb+srv://mukimbilla:alsk@1234@cluster0.x1mdc.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
 
 
 app.get("/", (req, res) => {
-    res.send("Hello Word")
-});
-
-app.get("/test", (req, res) => {
-    res.send("Test successfull")
-});
-
-app.get("/test2", (req, res) => {
-    res.send("Test2 also successfull")
+    res.send("Hello Word second")
 });
 
 
+
+
+
+
+
+
+
+let port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log("Listening to the port",port);
 })
